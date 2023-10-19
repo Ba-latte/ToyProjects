269864a832deddaf981c5bae7544992bc6f1b331
Bara <youj_lim@naver.com>
2023-10-19 22:58:16 +0900

    feat : 파티클 이동 중 화면 충돌시 방향 바꿈 기능 구현
    x, y방향으로 파티클이 움직이고 화면과 충돌하면 반대 방향으로 움직이는 기능 구현함

--- 06.moveGradient/index.js
+++ 06.moveGradient/index.js
@@ -12,9 +12,19 @@ let canvasHeight = innerHeight;
 const interval = 1000 / 60;
 
 // 총 파티클 갯수 : 화면 사이즈가 달라져도 비슷한 비율 유지하기
-const totalParticles = canvasWidth / 100;
+// const totalParticles = canvasWidth / 100;
+const totalParticles = 5;
+
 // 파티클 인스턴스 담을 배열
 const particles = [];
+
+// 색상
+const COLORS = [
+    {r: 45, g: 74, b: 227}, // blue
+    {r: 250, g: 255, b: 89}, // yellow
+    {r: 60, g: 176, b: 255}, // #3cb0ff
+    {r:255, g: 40, b: 87} // #ff2857
+];
 
 // 초기화 함수 : 처음 로드될 때, 리사이즈될 때 실행
 function init(){
@@ -33,6 +43,10 @@ function init(){
     // 기기마다 선명도 차별 적용
     ctx.scale(dpr, dpr);
 
+    // 색상 배열 갯수
+    let colorCount = 0;
+
+    // ctx.globalCompositeOperation = "saturation";
 
     // 파티클 생성
     for(let i = 0; i < totalParticles; i++ ){
@@ -40,8 +54,17 @@ function init(){
         const y = Math.random() * canvasHeight;
         const radius = Math.random() * (900, 400) + 400;
 
-        const particle = new Particle(x, y, radius);
+        // 파티클 만들기
+        const particle = new Particle(x, y, radius, COLORS[colorCount]);
 
+        // 색상 배열 갯수 1씩 증가시키기
+        colorCount = ++colorCount;
+        // console.log(colorCount);
+        
+        // 색상 배열 갯수보다 많아질 경우 0으로 재설정
+        if(colorCount >= COLORS.length) colorCount = 0;
+        
+        // 배열에 파티클 추가하기
         particles.push(particle);
     }
 }
@@ -67,8 +90,23 @@ function render(){
         
         // 파티클 관련
         for(let i = 0; i < totalParticles; i++){
+            // 파티클 업데이트
+            particles[i].update();
             // 파티클 그리기
             particles[i].draw(ctx);
+
+            // 충돌 감지
+            if(particles[i].isTouching(canvasWidth, canvasHeight) === "touchX"){
+                // console.log("X방향 충돌");
+                // 방향 바꾸기
+                particles[i].vx *= -1;
+                particles[i].x += particles[i].vx;
+            }
+            else if(particles[i].isTouching(canvasWidth, canvasHeight) === "touchY"){
+                // console.log("Y방향 충돌");
+                particles[i].vy *= -1;
+                particles[i].y += particles[i].vy;
+            }
         }
         
         ///////////////////////////////////////////////
